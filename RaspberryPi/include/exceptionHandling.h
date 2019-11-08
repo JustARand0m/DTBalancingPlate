4d4fc3ceaec902acbe7aec5cafa5bfe8a21e878e
JustARand0m <korbinian.federholzner@t-online.de>
2019-11-08 21:08:59 +0100

    added description to exception class

--- RaspberryPi/include/exceptionHandling.h
+++ RaspberryPi/include/exceptionHandling.h
@@ -1,8 +1,13 @@
 #include <iostream>
 
+/**
+ * Excption handling class.
+ * This function is used for expetion handling, the constructor gets the error Message
+ * which will than be accessable over the overloaded what() method
+ */
 class exceptionHandling: public std::exception {
 	public:
-		exceptionHandling(std::string _errorMsg) : errorMsg(_errorMsg) {}
+		exceptionHandling(std::string _errorMsg): errorMsg(_errorMsg) {}
 		const char* what() const throw() { return errorMsg.c_str(); }
 		~exceptionHandling() throw () {} 
 	private:
