2b32def0b9a5dc366c9ddecc01e2f47c8d9976ad
Korbinian1 Federholzner <fek47341@hs-regensburg.de>
2019-12-03 15:36:04 +0100

    adding description to adc interrupt routine

--- XMC/main.c
+++ XMC/main.c
@@ -44,7 +44,6 @@ void busy_waitMs(int ms)
 uint8_t read_data[BUFFERSIZE];
 uint8_t testdata[] = "testdata";
 uint32_t touchPanelResult;
-uint16_t x, y;
 
 int main(void)
 {
@@ -101,7 +100,11 @@ int main(void)
 }
 
 /**
+ * This interrupt routine gets executed when the ADC returns with an result after getting triggered with ADC_MEASUREMENT_StartConversion.
+ * After receiving the value of the x axis, the ADC needs be re-triggered to swap 2 pins to access the y axis and call this routine again.
+ * This routine sends the x and y coordinates via UART.
  *
+ * The PIN mapping on the Panel:
  * 2.15 => PIN 1 (UR) DIGITAL_IO_1
  * VDD 3.3V => PIN 2 (LR)
  * GND => PIN 4 (UL)
@@ -109,13 +112,15 @@ int main(void)
  */
 void Adc_Measurement_Handler(void)
 {
-	static uint16_t result_Channel_A = 0;
-    /* Read out conversion results */
-    result_Channel_A = ADC_MEASUREMENT_GetResult(&ADC_MEASUREMENT_Channel_A_handle);
+	static uint16_t x, y;
+
+	/* Read out conversion results */
+    uint16_t result_Channel_A = ADC_MEASUREMENT_GetResult(&ADC_MEASUREMENT_Channel_A_handle);
 
     if(DIGITAL_IO_GetInput(&DIGITAL_IO_1) == 1) {
     	x = result_Channel_A;
 
+    	/* Swap x and y axis on the panel */
         DIGITAL_IO_ToggleOutput(&DIGITAL_IO_0);
         DIGITAL_IO_ToggleOutput(&DIGITAL_IO_1);
 
@@ -124,11 +129,13 @@ void Adc_Measurement_Handler(void)
     } else {
     	y = result_Channel_A;
 
+    	/* Swap x and y axis on the panel */
         DIGITAL_IO_ToggleOutput(&DIGITAL_IO_0);
         DIGITAL_IO_ToggleOutput(&DIGITAL_IO_1);
 
-        // @todo -> trasmit result
-        //UART_Transmit(&UART_0, read_data, 8);
+        /* Transmit the x and y position via UART */
+        UART_Transmit(&UART_0, (uint8_t*)(&x), 2);
+        UART_Transmit(&UART_0, (uint8_t*)(&y), 2);
     }
 }
 
